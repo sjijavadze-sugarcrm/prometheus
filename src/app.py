f0565317877a77032e2cd08e77961dd80005f661
Sandro Jijavadze <sandro.jijavadze@sugarcrm.com>
2023-05-01 16:25:51 +0400

    add example labels

--- src/app.py
+++ src/app.py
@@ -5,13 +5,13 @@ from prometheus_client import Gauge
 app = Flask(__name__)
 metrics = PrometheusMetrics(app)
 
-topic_lag = Gauge('topic_lag', 'Kafka topic lag')
+topic_lag = Gauge('topic_lag', 'Kafka topic lag',  ['group_id', 'topic_name'])
 
 @app.route('/set_topic_lag')
 def set_topic_lag():
     value = request.args.get('value', type=float)
     if value is not None:
-        topic_lag.set(value)
+        topic_lag.labels("our_example_service", "example-topic").set(value)
         return f"Topic lag set to {value}"
     else:
         return "Invalid value", 400
